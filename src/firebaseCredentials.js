e069d10e2ef65bf45609570b8f0608f897993754
Tomás <bartoldit@gmail.com>
2023-01-30 02:14:19 -0300

    mini change2

--- src/firebaseCredentials.js
+++ src/firebaseCredentials.js
@@ -7,6 +7,32 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyBcbHlmj4107ywMJh5f4kmk0IlHqc2LAAU",
+  authDomain: "greydive-challenge-d23aa.firebaseapp.com",
+  projectId: "greydive-challenge-d23aa",
+  storageBucket: "greydive-challenge-d23aa.appspot.com",
+  messagingSenderId: "391290910704",
+  appId: "1:391290910704:web:c5eb94b21712e866088f35",
+  measurementId: "G-HYZ3GJ9D00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+/* // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyCHFsrB6T_sJQ5kNSXfjRSIet3q7WpwkGI",
   authDomain: "greydive-96b50.firebaseapp.com",
   projectId: "greydive-96b50",
@@ -17,7 +43,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); */
 
 
 
