0efa90efc1df60550b134ddf485a3adb4754c970
Tomás <bartoldit@gmail.com>
2023-01-30 12:13:27 -0300

    renovación de db

--- src/firebaseCredentials.js
+++ src/firebaseCredentials.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,33 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-
-
-
-
-/* // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCHFsrB6T_sJQ5kNSXfjRSIet3q7WpwkGI",
-  authDomain: "greydive-96b50.firebaseapp.com",
-  projectId: "greydive-96b50",
-  storageBucket: "greydive-96b50.appspot.com",
-  messagingSenderId: "243524370182",
-  appId: "1:243524370182:web:76a7f378c36ac75f1d2887",
-  measurementId: "G-MWSVHTZEB2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); */
-
 
 
 
